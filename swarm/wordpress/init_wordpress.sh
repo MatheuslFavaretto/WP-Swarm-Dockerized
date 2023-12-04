9dbea9fdb5567efbb40f9511de8cf0a39eb10c37
MatheuslFavaretto <matheusfavaretto.ef@gmail.com>
2023-12-04 08:21:44 -0300

    update swarm

--- swarm/wordpress/init_wordpress.sh
+++ swarm/wordpress/init_wordpress.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
-# Instalação do WordPress.
-wp core install \
+# Define o caminho do diretório do WordPress
+WP_PATH="/var/www/html"
+
+# Download e instalação do WordPress
+wp --allow-root core install \
   --path="/var/www/html" \
   --url="http://localhost:8080" \
   --title="Título do Blog" \
@@ -11,51 +14,51 @@ wp core install \
   --locale=pt_BR
 
 # Atualiza a estrutura de permalinks.
-wp option update permalink_structure /%postname%/ --skip-themes
+wp --allow-root option update permalink_structure /%postname%/ --skip-themes
 
 # Instala e ativa o tema Astra.
-wp theme install astra --activate
+wp --allow-root theme install astra --activate
 
 # Remove todos os temas (exceto o tema ativo).
-wp theme delete --all --force
+wp --allow-root theme delete --all --force
 
 # Desinstala todos os plugins.
-wp plugin uninstall --all
+wp --allow-root plugin uninstall --all
 
 # Instala e ativa o plugin Redis Object Cache.
-wp plugin install redis-cache --activate
+wp --allow-root plugin install redis-cache --activate
 
 # Configura o Redis Object Cache.
-wp config set --type=variable redis_host 'redis'
-wp config set --type=variable redis_port '6379'
-wp config set --type=constant WP_REDIS_HOST 'redis'
-wp config set --type=constant WP_REDIS_PORT '6379'
-wp config set --type=constant WP_REDIS_SELECTIVE_FLUSH 'true'
-wp config set --type=constant WP_CACHE 'true'
-wp redis enable
+wp --allow-root config set --type=variable redis_host 'redis'
+wp --allow-root config set --type=variable redis_port '6379'
+wp --allow-root config set --type=constant WP_REDIS_HOST 'redis'
+wp --allow-root config set --type=constant WP_REDIS_PORT '6379'
+wp --allow-root config set --type=constant WP_REDIS_SELECTIVE_FLUSH 'true'
+wp --allow-root config set --type=constant WP_CACHE 'true'
+wp --allow-root redis enable
 
 # Verifica o status da conexão do Redis.
-wp redis status
+wp --allow-root redis status
 
 # Configurações adicionais do plugin Redis.
-wp config set WP_REDIS_HOST "redis"
-wp config set WP_REDIS_PORT "6379"
-wp config set WP_REDIS_DATABASE "15"
+wp --allow-root config set WP_REDIS_HOST "redis"
+wp --allow-root config set WP_REDIS_PORT "6379"
+wp --allow-root config set WP_REDIS_DATABASE "15"
 
 # Define o fuso horário para São Paulo (America/Sao_Paulo).
-wp option update timezone_string "America/Sao_Paulo"
+wp --allow-root option update timezone_string "America/Sao_Paulo"
 
 # Exibe um relatório com lista de usuários, temas e plugins instalados.
 echo -e "\nRELATÓRIO\n"
 
 echo "== Lista de Usuários =="
-wp user list
+wp --allow-root user list --path="/var/www/html"
 echo ""
 
 echo "== Lista de Temas Instalados =="
-wp theme list
+wp --allow-root theme list --path="/var/www/html"
 echo ""
 
 echo "== Lista de Plugins Instalados =="
-wp plugin list
+wp --allow-root plugin list --path="/var/www/html"
 echo ""
